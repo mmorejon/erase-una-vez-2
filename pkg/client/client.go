e7988722c8854e5f33002c4155f4b1a24ea3b4f9
Manuel Morejon <manuel@mmorejon.io>
2020-01-18 16:22:44 +0100

    Remove http2 client configuration.
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,27 +1,10 @@
 package client
 
 import (
-	"crypto/tls"
-	"net"
 	"net/http"
-
-	"golang.org/x/net/http2"
 )
 
 // SetupClient is used to setup http client connection
 func SetupClient() *http.Client {
-	return &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-		Transport: &http2.Transport{
-			// So http2.Transport doesn't complain the URL scheme isn't 'https'
-			AllowHTTP: true,
-			// Pretend we are dialing a TLS endpoint.
-			// Note, we ignore the passed tls.Config
-			DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
-				return net.Dial(network, addr)
-			},
-		},
-	}
+	return &http.Client{}
 }
