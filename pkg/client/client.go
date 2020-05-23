0830c8a0fcb713fde56b37d9ea8447e1f016e8b2
Manuel Morejon <manuel@mmorejon.io>
2020-05-23 18:04:13 +0200

    Setup transport structure in the client.
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,10 +1,23 @@
 package client
 
 import (
+	"net"
 	"net/http"
+	"time"
 )
 
 // SetupClient is used to setup http client connection
 func SetupClient() *http.Client {
-	return &http.Client{}
+
+	return &http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   5 * time.Second,
+				KeepAlive: 5 * time.Second,
+			}).DialContext,
+			DisableKeepAlives:     true,
+			TLSHandshakeTimeout: 5 * time.Second,
+			ResponseHeaderTimeout: 5 * time.Second,
+		},
+	}
 }
